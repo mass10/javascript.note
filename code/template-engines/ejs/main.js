fd7bcf5606dd3d939259a7644f96e6a1eb72f123
mass10 <mass10.github@gmail.com>
2016-11-14 01:41:17 +0900

    wip

--- code/template-engines/ejs/main.js
+++ code/template-engines/ejs/main.js
@@ -5,28 +5,45 @@ function _startup() {
 
 }
 
+function handler_template_example1(req, res, next) {
+
+	//
+	// テンプレートファイルを利用してコンテンツを返却します。
+	//
+	var content = {name: "中臣鎌足"};
+	res.render("example1.ejs", content);
+}
+
+function handler_simple_json(req, res, next) {
+
+	//
+	// json
+	//
+	var content = {
+		status: "OK",
+		message_text: "Hello",
+		timestamp: new Date()
+	};
+
+	res.json(content);
+}
+
 function main() {
 
+	//
+	// setup
+	//
 	var express = require("express");
 	var ejs_template_engine = require("ejs")
 	var app = express();
 	app.set('view engine', 'ejs')
-	var server = app.listen(80, _startup);
+	var server = app.listen(8080, _startup);
+
 	console.log("Node.js is listening to port: " + server.address().port);
 
-	var handler_template_example1 = function(req, res, next) {
-		res.render("example1.ejs", {});
-	};
-
-	var handler_simple_json = function(req, res, next) {
-		var result = {
-			status: "OK",
-			message_text: "Hello",
-			timestamp: new Date()
-		};
-		res.json(result);
-	};
-
+	//
+	// routing
+	//
 	app.get("/hello", handler_simple_json);
 	app.get("/example1", handler_template_example1);
 }
