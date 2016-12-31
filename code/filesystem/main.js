03e6969e8e8e2d29fa160fa87be88a8a41d5d0bd
mass10 <mass10.github@gmail.com>
2016-12-31 11:31:43 +0900

    wip

---
+++ code/filesystem/main.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+// conding: utf-8
+
+function _test_entry(path) {
+
+	var fs = require("fs");
+	try {
+		var stat = fs.statSync(path);
+		if (stat.isFile()) {
+			console.log("[" + path + "] はファイルです。");
+		}
+		else if (stat.isDirectory()) {
+			console.log("[" + path + "] はディレクトリです。");
+		}
+		else {
+			// ???
+			console.log("[" + path + "] は不明なファイルシステムです。");
+		}
+	}
+	catch (err) {
+		// console.log(err);
+		console.log("[" + path + "] は不明なファイルシステムです。");
+	}
+}
+
+function _main(argv) {
+
+	if (argv.length <= 2) {
+		console.log("path ?");
+		return;
+	}
+
+	_test_entry(argv[2]);
+}
+
+_main(process.argv);
