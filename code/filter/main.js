48dfeea81cc818b6970d0ab423afbb21829c5712
Masaru Irisawa <mass10.github@gmail.com>
2018-11-09 15:40:44 +0900

    wip

--- code/filter/main.js
+++ code/filter/main.js
@@ -2,25 +2,28 @@
 // coding: utf-8
 
 function enum_addresses() {
-
 	const list = [
 		"東京都千代田区大手町1-1",
 		"東京都中野区中央2-15-1",
 		"山形県本庄市野口2-1",
-		"沖縄県那覇市辻2-3"
+		"沖縄県那覇市辻2-3",
+		null,
+		"",
+		"aaa"
 	];
 	return list;
 }
 
 function _filter_function(e) {
-	return 0 <= e.indexOf("沖縄県");
+	return 0 <= e.indexOf("県");
 }
 
 function main() {
-
-	const addresses = enum_addresses();
-	const filtered_addresses = addresses.filter(_filter_function)
-	console.log(filtered_addresses);
+	let addresses = enum_addresses();
+	// addresses = addresses.filter(_filter_function);
+	// addresses = addresses.filter(e => 0 <= e.indexOf("市"));
+	addresses = addresses.filter(Boolean);
+	console.log(addresses);
 }
 
 main();
