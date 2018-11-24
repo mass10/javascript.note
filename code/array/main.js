87f9a9e5a1f02002ec4c6373c590aeba3b38d4a7
mass10 <mass10.github@gmail.com>
2018-11-24 13:51:04 +0900

    wip

---
+++ code/array/main.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env nodejs
+// coding: utf-8
+
+function main() {
+
+	const ar = [111, 222, 333, 444, 555];
+	console.log(ar);
+	console.log(ar[-1]);
+
+	// console.log(ar[1:3]);
+}
+
+main();
+
